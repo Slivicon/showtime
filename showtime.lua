0146fe579bd33d13205da6422bdb5d6b36ebb12e
Tim MacAleese <tim@macaleese.ca>
2014-11-15 14:15:26 -0500

    use external xml config file

--- showtime.lua
+++ showtime.lua
@@ -1,10 +1,30 @@
-print("loading showtime15");
+--
+-- showtime15 - Displays the real world time within Farming Simulator 15
+--
+-- @author  Slivicon https://github.com/Slivicon/showtime
+-- @date    2014-11-11
+-- Thank you to:
+--  Decker_MMIV for code to read and write config XML and utility http://fs-uk.com/mods/list/user/90139
+--  Marhu for code to dynamically determine clock position http://marhu.net
+--
 
 showtime = {};
 
+local modItem = ModsUtil.findModItemByModName(g_currentModName);
+showtime.version = (modItem and modItem.version) and modItem.version or "?.?.?";
+
 function showtime:loadMap(name)
-	fontSize = 0.018;
-	timeFormat = "%H:%M";
+  if g_client == nil then
+    return;
+  end;
+  showtime.setDefaults();
+  local fXmlName = g_modsDirectory .. "/" .. "showtime.xml";
+  local tag = "showtime";
+  if not fileExists(fXmlName) then
+    showtime.writeConfig(fXmlName, tag);
+  else
+    showtime.readConfig(fXmlName, tag);
+  end;
 end;
 
 function showtime:deleteMap()
@@ -24,21 +44,169 @@ function showtime:update(dt)
 end;
 
 function showtime:draw()
-	if not g_currentMission.renderTime then
-		return;
-	end;
-	--set text params
-	setTextColor(0, 0, 0, 1);
-	setTextAlignment(RenderText.ALIGN_RIGHT);
-	--improved position based on Marhu's better resolution-independent code
-	local xPos = g_currentMission.weatherTimeBackgroundOverlay.x + g_currentMission.weatherTimeBackgroundWidth - g_currentMission.timeOffsetRight;
-	local yPos = g_currentMission.weatherTimeBackgroundOverlay.y + (fontSize/4);
-	local sTime = tostring(getDate(timeFormat));
-	renderText(xPos, yPos, fontSize, sTime);
-	--renderText(0.87, 0.91, 0.018, getDate("%H:%M")); --static position values which have a different result depending on screen resolution
-	--reset text params
-	setTextColor(1, 1, 1, 1);
-	setTextAlignment(RenderText.ALIGN_LEFT);
+  if not g_currentMission.renderTime then
+    return;
+  end;
+  setTextColor(0, 0, 0, 1);
+  setTextAlignment(RenderText.ALIGN_RIGHT);
+  local posX = showtime.posX;
+  local posY = showtime.posY;
+  if showtime.posDynamic then
+    posX = showtime.getDefaultPosX();
+	posY = showtime.getDefaultPosY(showtime.fontSize);
+  end
+  renderText(posX, posY, showtime.fontSize, tostring(getDate(showtime.timeFormat)));
+  setTextColor(1, 1, 1, 1);
+  setTextAlignment(RenderText.ALIGN_LEFT);
 end;
 
+function showtime.getDefaultFontSize()
+  return 0.018;
+end
+
+function showtime.getDefaultPosDynamic()
+  return true;
+end
+
+function showtime.getDefaultPosX()
+  return g_currentMission.weatherTimeBackgroundOverlay.x + g_currentMission.weatherTimeBackgroundWidth - g_currentMission.timeOffsetRight;
+end
+
+function showtime.getDefaultPosY(fontSize)
+  if fontSize == nil then
+    fontSize = showtime.getDefaultFontSize();
+  end
+  return g_currentMission.weatherTimeBackgroundOverlay.y + (fontSize/4);
+end
+
+function showtime.getDefaultShowSeconds()
+  return false;
+end
+
+function showtime.getDefaultTimeFormat24()
+  return true;
+end
+
+function showtime.getFontSize(fontSize)
+  if showtime.isValidFloat(fontSize) then
+    return fontSize;
+  end
+  return showtime.getDefaultFontSize();
+end
+
+function showtime.getPosDynamic(posDynamic)
+  if posDynamic then
+    return true;
+  end
+  return false;
+end
+
+function showtime.getPosX(posX)
+  if showtime.isValidFloat(posX) then
+    return posX;
+  end
+  return showtime.getDefaultPosX();
+end
+
+function showtime.getPosY(posY)
+  if showtime.isValidFloat(posY) then
+    return posY;
+  end
+  return showtime.getDefaultPosY();
+end
+
+function showtime.getTimeFormat(timeFormat24, showSeconds)
+  local timeFormat = "";
+  if timeFormat24 then
+    timeFormat = "%H";
+  else
+    timeFormat = "%I";
+  end;
+  timeFormat = timeFormat .. ":%M";
+  if showSeconds then
+    timeFormat = timeFormat .. ":%S";
+  end;
+  return timeFormat;
+end
+
+function showtime.getTimeFormat24(timeFormat24)
+  if timeFormat24 then
+    return true;
+  end
+  return false;
+end
+
+function showtime.isValidConfig(fXml, tag)
+  if fXml == nil then
+    return false;
+  end;
+  local configItems = {fontSize = fontSize, posDynamic = posDynamic, posX = posX, posY = posY, timeFormat24 = timeFormat24, showSeconds = showSeconds}
+  for k, v in pairs(configItems) do
+    if not hasXmlProperty(fXml, tag .. "#" .. k) then
+	  return false;
+	end;
+  end;
+  return true;
+end;
+
+function showtime.isValidFloat(num)
+  e = tonumber(num);
+  if e == nil then
+    return false;
+  end
+  if e > 0 and e < 1 then
+    return true;
+  end
+  return false;
+end
+
+function showtime.readConfig(fXmlName, tag)
+  local fXml = loadXMLFile(tag, fXmlName, tag);
+  if not showtime.isValidConfig(fXml, tag) then
+    return;
+  end
+  local fontSize = getXMLFloat(fXml, tag .. "#fontSize");
+  local posDynamic = getXMLBool(fXml, tag .. "#posDynamic");
+  local posX = getXMLFloat(fXml, tag .. "#posX");
+  local posY = getXMLFloat(fXml, tag .. "#posY");
+  local timeFormat24 = getXMLBool(fXml, tag .. "#timeFormat24");
+  local showSeconds = getXMLBool(fXml, tag .. "#showSeconds");
+  showtime.setParams(fontSize, posDynamic, posX, posY, timeFormat24, showSeconds);
+  delete(fXml);
+end
+
+function showtime.setDefaults()
+  local fontSize = showtime.getDefaultFontSize();
+  showtime.setParams(fontSize, showtime.getDefaultPosDynamic(), showtime.getDefaultPosX(), showtime.getDefaultPosY(fontSize), showtime.getDefaultTimeFormat24(), showtime.getDefaultShowSeconds());
+end
+
+function showtime.setParams(fontSize, posDynamic, posX, posY, timeFormat24, showSeconds)
+  showtime.fontSize = showtime.getFontSize(fontSize);
+  showtime.posDynamic = showtime.getPosDynamic(posDynamic);
+  showtime.posX = showtime.getPosX(posX);
+  showtime.posY = showtime.getPosY(posY);
+  showtime.timeFormat24 = showtime.getTimeFormat24(timeFormat24);
+  showtime.timeFormat = showtime.getTimeFormat(timeFormat24, showSeconds);
+end
+
+function showtime.writeConfig(fXmlName, tag)
+  local fXml = createXMLFile(tag, fXmlName, tag);
+  setXMLFloat(fXml, tag .. "#fontSize", showtime.fontSize);
+  setXMLBool(fXml, tag .. "#posDynamic", showtime.posDynamic);
+  local posX = showtime.posX;
+  local posY = showtime.posY;
+  if showtime.posDynamic then
+    posX = 0;
+	posY = 0;
+  end
+  setXMLFloat(fXml, tag .. "#posX", posX);
+  setXMLFloat(fXml, tag .. "#posY", posY);
+  setXMLBool(fXml, tag .. "#timeFormat24", showtime.timeFormat24);
+  setXMLBool(fXml, tag .. "#showSeconds", showtime.showSeconds);
+  saveXMLFile(fXml);
+  delete(fXml);
+end
+
 addModEventListener(showtime);
+
+print(string.format("Script loaded: showtime.lua (v%s)", showtime.version));
